d7acb68f56bc27fce878619e9ee2ec376ac9c74d
Mikael Koskinen <mijapkos@gmail.com>
2012-03-23 15:25:11 +0200

    Added 404

--- generate.ps1
+++ generate.ps1
@@ -23,6 +23,8 @@ try {
 
 	copy sitemap.xml public
 
+	.\graze.exe -t .\root\template -tf .\root\template\404.cshtml -of .\public\404.html
+
 	exit 0 # Success
 } catch {
 	echo "Generating sites failed"
